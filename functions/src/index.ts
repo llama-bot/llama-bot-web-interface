84bcda3f412f0c2159f5ee93241d1672d692ab99
developomp <developomp@gmail.com>
2021-09-13 10:45:28 +0900

    potential fix for project not working on production

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,42 +2,40 @@ import admin from "firebase-admin"
 import { https } from "firebase-functions"
 
 import express from "express"
-import expressSession from "express-session"
-import passport from "passport"
 import cors from "cors"
+import session from "express-session"
+import passport from "passport"
 
 import authRoutes from "./routes/authRoutes"
 import dataRoutes from "./routes/dataRoutes"
 
-import secret from "./secret.json"
-
-import serviceAccountKey from "./firebase-adminsdk.json"
-
 import "./services/discordOauth"
 
-const app = express()
-
-app.set("trust proxy", 1)
-app.use(
-	cors({ origin: ["https://llama.developomp.com", "http://localhost:5000"] })
-)
-app.use(
-	expressSession({
-		secret: secret.session,
-		resave: true,
-		saveUninitialized: true,
-	})
-)
-app.use(passport.initialize())
-app.use(passport.session())
-
-dataRoutes(app)
-authRoutes(app)
+import secret from "./secret.json"
+import serviceAccountKey from "./firebase-adminsdk.json"
 
 admin.initializeApp({
 	credential: admin.credential.cert(
 		serviceAccountKey as admin.ServiceAccount
 	),
 })
+
+const app = express()
+
+app.use((_, res, next) => {
+	res.setHeader("Cache-Control", "private")
+	next()
+})
+app.use(
+	cors({ origin: ["https://llama.developomp.com", "http://localhost:5000"] })
+)
+app.use(
+	session({ secret: secret.session, resave: false, saveUninitialized: false })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+dataRoutes(app)
+authRoutes(app)
 
 export const api = https.onRequest(app)
